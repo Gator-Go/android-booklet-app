19a051c678479a78f660eed77532be05b7444a69
Gator-Go <gator@sw-builder.com>
2026-08-17 11:21:58 -0400

    logo fix

--- Extender/BookletExtender.groovy
+++ Extender/BookletExtender.groovy
@@ -55,25 +55,25 @@ def extFiles ( theDir, changes ) {
 extFiles ( dir, changes )
 
 def src = new File("./logo.png").newDataInputStream()
-def dst = new File("../security/app/src/main/res/drawable/logo.png").newDataOutputStream()
+def dst = new File("../booklet/app/src/main/res/drawable/logo.png").newDataOutputStream()
 dst << src
 
 def src1 = new File("./hdpi-logo.png").newDataInputStream()
-def dst1 = new File("../security/app/src/main/res/drawable-hdpi/logo.png").newDataOutputStream()
+def dst1 = new File("../booklet/app/src/main/res/drawable-hdpi/logo.png").newDataOutputStream()
 dst1 << src1
 
 def src2 = new File("./ldpi-logo.png").newDataInputStream()
-def dst2 = new File("../security/app/src/main/res/drawable-ldpi/logo.png").newDataOutputStream()
+def dst2 = new File("../booklet/app/src/main/res/drawable-ldpi/logo.png").newDataOutputStream()
 dst2 << src2
 
 def src3 = new File("./mdpi-logo.png").newDataInputStream()
-def dst3 = new File("../security/app/src/main/res/drawable-mdpi/logo.png").newDataOutputStream()
+def dst3 = new File("../booklet/app/src/main/res/drawable-mdpi/logo.png").newDataOutputStream()
 dst3 << src3
 
 def src4 = new File("./xhdpi-logo.png").newDataInputStream()
-def dst4 = new File("../security/app/src/main/res/drawable-xhdpi/logo.png").newDataOutputStream()
+def dst4 = new File("../booklet/app/src/main/res/drawable-xhdpi/logo.png").newDataOutputStream()
 dst4 << src4
 
 def src5 = new File("./xxhdpi-logo.png").newDataInputStream()
-def dst5 = new File("../security/app/src/main/res/drawable-xxhdpi/logo.png").newDataOutputStream()
+def dst5 = new File("../booklet/app/src/main/res/drawable-xxhdpi/logo.png").newDataOutputStream()
 dst5 << src5
