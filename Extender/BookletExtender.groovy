ecd6a52a13d81ffbb17a808d5a1b8ef0e9b7994c
Gator-Go <gator@sw-builder.com>
2026-08-17 11:16:51 -0400

    add logos to repo

--- Extender/BookletExtender.groovy
+++ Extender/BookletExtender.groovy
@@ -16,78 +16,6 @@ MyData newData = new MyData()
 
 
 //
-// Post list changes
-//
-
-
-newData = new MyData()
-newData.theFile = "SentenceDao.java"
-newData.extMarker =
-"""
-  private String orderBy = "LAST_UPDATE DESC";
-"""
-newData.srcInsert =
-"""
-  private String orderBy = "_ID ASC";
-"""
-changes.add(newData)
-
-
-
-newData = new MyData()
-newData.theFile = "SentenceAdd.java"
-newData.extMarker =
-"""
-    if (sentenceTextEdit.getText().toString().trim().length() > 0)
-       sentence.setSentenceText(sentenceTextEdit.getText().toString());
-    else
-"""
-newData.srcInsert =
-"""
-    if (sentenceTextEdit.getText().toString().trim().length() > 0) {
-       String sentenceText = sentenceTextEdit.getText().toString() + ".";
-       String sentenceTextCap = sentenceText.substring(0, 1).toUpperCase() + sentenceText.substring(1);
-       sentence.setSentenceText(sentenceTextCap);
-    } else
-"""
-changes.add(newData)
-
-
-
-newData = new MyData()
-newData.theFile = "sentence_add.xml"
-newData.extMarker =
-"""
-            android:maxLength="255"
-            android:inputType="textFilter"
-            android:singleLine="true" />
-"""
-newData.srcInsert =
-"""
-            android:inputType="textMultiLine"
-            android:maxLines="3" />
-"""
-changes.add(newData)
-
-
-
-newData = new MyData()
-newData.theFile = "activity.xml"
-newData.extMarker =
-"""
-            android:drawableTop="@drawable/socialBook_help"
-"""
-newData.srcInsert =
-"""
-            android:drawableTop="@drawable/social_book_help"
-"""
-changes.add(newData)
-
-
-
-
-
-//
 // Sync file change
 // 
 def dir = "../booklet"
@@ -126,7 +54,26 @@ def extFiles ( theDir, changes ) {
 
 extFiles ( dir, changes )
 
+def src = new File("./logo.png").newDataInputStream()
+def dst = new File("../security/app/src/main/res/drawable/logo.png").newDataOutputStream()
+dst << src
 
+def src1 = new File("./hdpi-logo.png").newDataInputStream()
+def dst1 = new File("../security/app/src/main/res/drawable-hdpi/logo.png").newDataOutputStream()
+dst1 << src1
 
-//def strings = new File("./strings.xml").text
-//new File("../booklet/app/src/main/res/values/strings.xml").write(strings)
+def src2 = new File("./ldpi-logo.png").newDataInputStream()
+def dst2 = new File("../security/app/src/main/res/drawable-ldpi/logo.png").newDataOutputStream()
+dst2 << src2
+
+def src3 = new File("./mdpi-logo.png").newDataInputStream()
+def dst3 = new File("../security/app/src/main/res/drawable-mdpi/logo.png").newDataOutputStream()
+dst3 << src3
+
+def src4 = new File("./xhdpi-logo.png").newDataInputStream()
+def dst4 = new File("../security/app/src/main/res/drawable-xhdpi/logo.png").newDataOutputStream()
+dst4 << src4
+
+def src5 = new File("./xxhdpi-logo.png").newDataInputStream()
+def dst5 = new File("../security/app/src/main/res/drawable-xxhdpi/logo.png").newDataOutputStream()
+dst5 << src5
